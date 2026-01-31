37f9fdb2241e92fd365ee5a4c216a9731ecf63cc
harshitaslayss <harshitasharma9625@gmail.com>
2026-02-01 02:41:06 +0530

    edited

--- cloudinary_upload.py
+++ cloudinary_upload.py
@@ -1,10 +1,11 @@
+import os
 import cloudinary
 import cloudinary.uploader
 
 cloudinary.config(
-    cloud_name="dciqxvk5p",
-    api_key="165463927626715",
-    api_secret="ef-FvlQpDTIalwIF8tmj5UX7faY",
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
     secure=True
 )
 
